7a2171bfebb8a5af0dab8f46c0db02cd3125dc2c
evgmolt <evg.molt@gmail.com>
2021-08-25 12:05:39 +0400

    Router, AppBar and Profile added

---
+++ lesson2/src/Chat/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import MessageListComponent from "./MessageListComponent";
+import ChatListComponent from "../ChatListComponent";
+import InputWrapperComponent from "./InputWrapperComponent"
+import { makeStyles } from '@material-ui/core/styles';
+
+function Chat() {
+
+  const [messagesArray, setMessagesArray] = useState([]);
+  
+  const onSendMessage= (messageText, author) => {
+    setMessagesArray(prev => [...prev, {
+      messageText,
+      author : author,
+      }
+    ]);
+  }
+  
+  const useStyles = makeStyles(() => ({
+    chatWrapper: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    componentWrapper: {
+        height: "500px",
+        width: "600px",
+        border: "1px solid black",
+        display: "flex",
+        flexDirection: "column"
+    },
+    lists: {
+      height : "90%"
+    }
+}));
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (messagesArray.length > 0) {
+      const botMessage = "Сообщение получено";
+      const botAuthor = "bot";
+      const lastMessage = messagesArray[messagesArray.length - 1];
+      if (lastMessage.author !== botAuthor) {
+        setTimeout(() => onSendMessage(botMessage, botAuthor), 1500);
+      }
+    }
+  }, [messagesArray])
+
+  let chatsArray = [{ id : "0", name : "Name 1"}, { id : "2", name : "Name 2"}];
+
+  return (
+    <div className={classes.chatWrapper}>
+      <div className={classes.componentWrapper}>
+        <div className={classes.lists}>
+        <ChatListComponent array={chatsArray}/>
+        <MessageListComponent array= {messagesArray}/>
+        </div>
+        <InputWrapperComponent onSendMessage = {onSendMessage}/>
+      </div>
+    </div>
+  );
+} 
+
+export default Chat;
